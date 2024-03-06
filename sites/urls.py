d8683e7c2c29de393a1ecf1fef062d44509ee780
bhaitabahi786 <bhaitabahi786@gmail.com>
2024-03-06 23:29:53 +0530

    AddSite and addmanpower created workingfine

--- sites/urls.py
+++ sites/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     path('addSites/', views.addSites, name='addSites'),
     path('ManageSites/<int:site_id>', views.ManageSites, name='ManageSites'),
     path('ManageSites/AddExpenses/', views.AddExpenses, name='AddExpenses'),
-    path('ManageSites/AddManpower/', views.AddManpower, name='AddManpower'),
+    path('ManageSites/AddManpower/<int:site_id>', views.AddManpower, name='AddManpower'),
     path('ManageSites/AddTools/', views.AddTools, name='AddTools'),
     path('ManageSites/LabourDetails/', views.LabourDetails, name='LabourDetails'),
     path('delete_site/<int:site_id>/', views.delete_site, name='delete_site'),
+    path('delete_Manpower/<int:man_id>/<int:site_id>', views.delete_Manpower, name='delete_Manpower'),
         
 ]
 
--- sites/views.py
+++ sites/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404,redirect
-from .models import Site
+from .models import *
 
 # Create your views here.
 
@@ -10,16 +10,40 @@ def sites(request):
     return render(request, 'sites/sitepage.html', {'sites': sites})
 
 def addSites(request):
+
+    if request.method == 'POST':
+        site_name = request.POST.get('site_name')
+        site_address = request.POST.get('site_address')
+        quotation_amount = request.POST.get('quotation_amount')
+        
+        SiteAdd = Site(name=site_name, address=site_address, quotation_amount=quotation_amount)
+        SiteAdd.save()
+        return redirect('sites')
+
     return render(request,'sites/AddSitePage.html')
 
 def ManageSites(request,site_id):
     siteInfo = get_object_or_404(Site, pk=site_id)
-    print(siteInfo)
 
-    return render(request,'sites/ManageSitePage.html',{'siteInfo':siteInfo})
+    manpowers = Manpower.objects.all()
 
-def AddManpower(request):
-    return render(request,'sites/AddManpowerPage.html')
+    return render(request,'sites/ManageSitePage.html',{'siteInfo':siteInfo,'manpowers':manpowers})
+
+def AddManpower(request,site_id):
+    siteInfo = get_object_or_404(Site, pk=site_id)
+
+    if request.method == 'POST':
+        site_name = request.POST.get('site_name')
+        site_name2 = Site.objects.get(name=site_name)
+        emp_name = request.POST.get('emp_name')
+        aadhar_no = request.POST.get('aadhar_no')
+        contact = request.POST.get('contact')
+        
+        ManpowerAdd = Manpower(site=site_name2, name=emp_name, adhaarno=aadhar_no, contact=contact)
+        ManpowerAdd.save()
+        return redirect('ManageSites',site_id=site_id)
+
+    return render(request,'sites/AddManpowerPage.html',{'siteInfo':siteInfo})
 
 def AddExpenses(request):
     return render(request,'sites/AddExpensesPage.html')
@@ -34,5 +58,10 @@ def delete_site(request, site_id):
     site = get_object_or_404(Site, pk=site_id)
     site.delete()
     return redirect('sites')
+
+def delete_Manpower(request, man_id, site_id):
+    manpower = get_object_or_404(Manpower, pk=man_id)
+    manpower.delete()
+    return redirect('ManageSites',site_id=site_id)
 
 
